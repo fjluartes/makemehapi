50bd75ab797133417d7644e3e0e3cc88330d9905
fjgl <fjluartes@gmail.com>
2018-09-13 18:24:49 +0800

    Adds final solution for val joi object

--- validation-joi-obj/program.js
+++ validation-joi-obj/program.js
@@ -2,30 +2,44 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Joi = require('joi');
 
 const server = new Hapi.server({
-    host: 'localhost',
-    port: Number(process.argv[2] || 8080)
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
 });
 
 const myHandler = function(request, h) {
-    return 'Hello';
+  return 'login successful';
 }
 
 const init = async () => {
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: myHandler
-    });
+  server.route({
+    method: 'POST',
+    path: '/login',
+    handler: myHandler,
+    options: {
+      validate: {
+        payload: Joi.object({
+          isGuest: Joi.boolean().required(),
+          username: Joi.string().when('isGuest', {
+            is: false, then: Joi.required()
+          }),
+          password: Joi.string().alphanum(),
+          accessToken: Joi.string().alphanum()
+        }).options({allowUnknown: true})
+          .without('password', 'accessToken')
+      }
+    }
+  });
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 }
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
