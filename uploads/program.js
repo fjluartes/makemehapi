251dfe623d8e8626522cb67ca027c6975db561d0
fjgl <fjluartes@gmail.com>
2018-09-14 14:37:22 +0800

    Add boom to package.json

--- uploads/program.js
+++ uploads/program.js
@@ -4,56 +4,56 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.server({
-    host: 'localhost',
-    port: Number(process.argv[2] || 8080)
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
 });
 
 const myHandler = function(request, h) {
-    return new Promise((resolve, reject) => {
-        let body = '';
-        request.payload.file.on('data', (data) => {
-            body += data;
-        });
-        
-        request.payload.file.on('end', () => {
-            let result = {
-                description: request.payload.description,
-                file: {
-                    data: body, 
-                    filename: request.payload.file.hapi.filename,
-                    headers: request.payload.file.hapi.headers
-                }
-            };
-            return resolve(JSON.stringify(result));
-        });
-
-        request.payload.file.on('error', (err) => {
-            return reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    let body = '';
+    request.payload.file.on('data', (data) => {
+      body += data;
     });
+    
+    request.payload.file.on('end', () => {
+      let result = {
+        description: request.payload.description,
+        file: {
+          data: body, 
+          filename: request.payload.file.hapi.filename,
+          headers: request.payload.file.hapi.headers
+        }
+      };
+      return resolve(JSON.stringify(result));
+    });
+
+    request.payload.file.on('error', (err) => {
+      return reject(err);
+    });
+  });
 }
 
 const init = async () => {
-    server.route({
-        method: 'POST',
-        path: '/upload',
-        handler: myHandler,
-        options: {
-            payload: {
-                output: 'stream',
-                parse: true,
-                allow: 'multipart/form-data'
-            }
-        }
-    });
+  server.route({
+    method: 'POST',
+    path: '/upload',
+    handler: myHandler,
+    options: {
+      payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data'
+      }
+    }
+  });
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 }
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
